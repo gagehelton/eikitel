73a97d192c74dbc17cdb2c18f348abf0afd7dff8
Gage Helton <ghelton@sudotronics.com>
2021-04-15 21:11:16 +0000

    prep for site test

--- eiki.py
+++ eiki.py
@@ -1,13 +1,11 @@
 from telnetlib import Telnet
-from helpers import required_args
+from helpers import required_args,lineno
 from logg3r import Log
 import json
 
-_LOG_PATH = "./test_logs/"
-_INIT_LOGGER = Log(log_path=_LOG_PATH,name='init'.format(**self.__dict__),level=1)
-_PORT = 3023
-_TIMEOUT = 5
-_COMMANDS = json.load(open("./commands.json","r"))
+config = json.load(open("./config.json","r"))
+
+_INIT_LOGGER = Log(log_path=config['log_path'],name='init',level=1)
 
 class Eiki():
     def __init__(self,**kwargs):
@@ -15,12 +13,12 @@ class Eiki():
         success,error = required_args(kwargs,required)
         if(success):
             self.__dict__.update(**kwargs)
-            self.logger = Log(log_path=_LOG_PATH,
+            self.logger = Log(log_path=config['log_path'],
                                 name=self.name+"_"+self.ip.replace(".","-"),
                                 level=1)
             self.session = False
             self.state = -1 #-1 = un-set value or other error
-            self.control(3)
+            self.control(command="status")
         else:
             _INIT_LOGGER.log(lineno()+"ERROR: {}".format(error),5)            
 
@@ -28,21 +26,21 @@ class Eiki():
         required = {"command":str}
         success,error = required_args(kwargs,required)
         if(success):
-            if(kwargs['command'] in _COMMANDS):
-            self.session = Telnet(self.ip,port=_PORT,timeout=_TIMEOUT)
-            if(self.session):
-                self.session.write(b'{}\n\r'.format(_COMMANDS['command']))
-                #READ HERE AND SET PROJECTOR STATE 
-                #SEND REQUEST FOR VALUE HERE... IDK
-                self.session.close()
-                return True #ACTUAL PROJECTOR STATE 0=off 1=on -1=error
-            else:
-                self.logger.log(lineno()+"Connection Failed! {}".format(self.session),5)
-                return -1
+            if(kwargs['command'] in config['commands']):
+                self.session = Telnet(self.ip,port=config['port'],timeout=config['timeout'])
+                if(self.session):
+                    self.session.write(b'{}\n\r'.format(config['commands'][kwargs['command']]))
+                    #READ HERE AND SET PROJECTOR STATE 
+                    #SEND REQUEST FOR VALUE HERE... IDK
+                    self.session.close()
+                    return True #ACTUAL PROJECTOR STATE 0=off 1=on -1=error
+                else:
+                    self.logger.log(lineno()+"Connection Failed! {}".format(self.session),5)
+                    return -1
         else:
             self.logger.log(lineno()+"ERROR: {}".format(error),5)
             return -1
 
 if __name__ == "__main__":
-    x = projector(ip="192.168.1.2",name="front")
+    x = Eiki(ip="192.168.1.2",name="front")
     print(x.__dict__)
